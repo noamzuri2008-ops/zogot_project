a899f3caa58157296460c3a981bae10491cc7066
jbt <galgalosha@gmail.com>
2026-09-09 09:57:03 +0300

    close window after close timeout

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main() -> None:
     game_field.create_mines()
     game_field.create_flag()
 
+    end_game_timeout: float | int = time.time()
 
     while state["is_running"]:
         if time.time() - state["xray_start_time"] > 1:
@@ -27,6 +28,11 @@ def main() -> None:
             state["state"] = consts.LOSE_STATE
         if game_field.check_soldier_flag():
             state["state"] = consts.WIN_STATE
+
+        if state["state"] == consts.RUNNING_STATE:
+            end_game_timeout = time.time()
+        elif time.time() - end_game_timeout > consts.END_GAME_TIMEOUT:
+                break
 
         screen.draw_game(state)
 
@@ -39,6 +45,7 @@ def set_game_state() -> None:
         "is_xray": False,
         "xray_start_time": time.time(),
     }
+
 
 def handle_user_events() -> None:
     for event in pygame.event.get():
@@ -65,7 +72,6 @@ def handle_user_events() -> None:
                 soldier.move_left()
 
             game_field.update_solider()
-
 
 
 if __name__ == '__main__':
