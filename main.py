aea2ff3c46446740b62d1cb38460019eb3533a59
jbt <galgalosha@gmail.com>
2026-09-09 11:53:48 +0300

    save and load events

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import time
 from typing import Any
 import pygame
+
+import save_games
 import soldier
 import consts
 import screen
 import game_field
 
 state: dict[str, Any] = dict()
-
+pygame_number_keys: list[int] = [
+    pygame.K_1,
+    pygame.K_2,
+    pygame.K_3,
+    pygame.K_4,
+    pygame.K_5,
+    pygame.K_6,
+    pygame.K_7,
+    pygame.K_8,
+    pygame.K_9,
+]
 
 def main() -> None:
     pygame.init()
@@ -44,6 +56,9 @@ def set_game_state() -> None:
         "is_running": True,
         "is_xray": False,
         "xray_start_time": time.time(),
+        "pressed_loading_saving_num": -1,
+        "time_loading_saving_num_pressed" : time.time()
+
     }
 
 
@@ -56,10 +71,25 @@ def handle_user_events() -> None:
         elif not state["state"] == consts.RUNNING_STATE:
             continue
 
+        if event.type == pygame.KEYDOWN and event.key in pygame_number_keys:
+            state["timestamp_loading_saving_num_pressed"] = time.time()
+            state["pressed_loading_saving_num"] = event.key
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RETURN and not state["is_xray"]:
                 state["is_xray"] = True
                 state["xray_start_time"] = time.time()
+
+            if event.key == state["pressed_loading_saving_num"]:
+                save_load_num_key_index: int = pygame_number_keys.index(event.key)
+                if time.time() - state["timestamp_loading_saving_num_pressed"] > consts.NUM_OF_SECONDS_LOAD_NUM_PRESS:
+                    # TODO load game functions
+                    # save_games.load_game(save_load_num_key_index)
+                    ...
+                else:
+                    # TODO save game functions
+                    # save_games.save_game(save_load_num_key_index)
+                    ...
 
             game_field.remove_soldier()
             if event.key == pygame.K_UP:
